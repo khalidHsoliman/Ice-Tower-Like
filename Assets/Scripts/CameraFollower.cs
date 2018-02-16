6e93ff5a8eebdcbd971c6d04e1923e5d26301da1
Khalid Hassan <khalidhsoliman@gmail.com>
2018-02-16 22:56:09 +0200

    ModifyCameraFollower

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour {
 
 	public Transform target; 
+	public float smoothTime = 1f; 
 
-	float _offset; 
-
-	Vector3 m_CurrentVelocity;
-	Vector3 m_LastTargetPosition;
-	Vector3 m_LookAheadPos; 
+	Vector3 _currentVelocity; 
 	
 	void Awake () {
 		if(target == null)
@@ -19,27 +16,15 @@ public class CameraFollower : MonoBehaviour {
 		}
 	}
 
-	void Start() {
-		_offset = (transform.position - target.position).z; 
-
-		m_LastTargetPosition = transform.position; 
-	}
-	
-	// Update is called once per frame
 	void Update () {
-			if (target == null)
-				return;
+	
+		if (target == null)
+			return;
 
-            float xMoveDelta = (target.position - m_LastTargetPosition).x;
-
-			m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime);
-
-            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*_offset;
-			
-            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, 0f);
-
-            transform.position = newPos;
-
-            m_LastTargetPosition = target.position;
+		if(target.position.y > transform.position.y)
+		{
+			Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.y); 
+			transform.position = Vector3.SmoothDamp(transform.position, newPos, ref _currentVelocity, smoothTime); 
+		}
 	}
 }
