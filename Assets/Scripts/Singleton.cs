efca998dc6ad92cb5bb92bdfe86cf56494b7dc54
Khalid Hassan <khalidhsoliman@gmail.com>
2018-02-18 22:46:17 +0200

    Add Singleton Script
    add singleton pattern so that my game manger inherits from

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+
+    #region Singleton
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = GameObject.FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject(typeof(T).Name);
+                        _instance = singleton.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+#endregion 
