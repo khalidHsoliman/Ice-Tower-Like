e253a85addbd0b7c0ec44edd4fc43f1441034d66
Khalid Hassan <khalidhsoliman@gmail.com>
2018-02-18 22:56:01 +0200

    Modify Platform Script
    force the player up when collision happens

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour, IPooledObject {
+public class Platform : MonoBehaviour {
 
-	public void OnObjectSpawn()
+	public float upForce = 10f; 
+	
+	void OnCollisionEnter2D(Collision2D collision)
 	{
-		
+		if(collision.relativeVelocity.y <= 0f)
+		{
+			Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>(); 		
+			if(rb != null)
+			{
+				Vector2 velocity = new Vector2(); 
+				velocity = rb.velocity; 
+				velocity.y = upForce; 
+				rb.velocity = velocity;
+			}
+		}	
 	}
 }
