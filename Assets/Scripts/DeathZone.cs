00213a9479b0dc3169ee58f949b665acd34bcd74
Khalid Hassan <khalidhsoliman@gmail.com>
2018-02-18 22:54:27 +0200

    Add DeathZone Script
    Death Zone attached to the bottom of the camera to detect the player falling

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.gameObject.CompareTag("Player"))
+		{
+			if(GameManager.Instance)
+			{
+				if(!GameManager.Instance.gameIsOver)
+					collider.gameObject.GetComponent<PlayerController>().KillPlayer(); 
+			}
+		}
+	}
+}
